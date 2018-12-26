91f39d85ecf5207061433a7a9242655e8e20add8
yagoubigithub <yagoubi.aek.2@gmail.com>
2018-12-26 22:57:20 +0100

    26/12/2018 22:57

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
-import getPicture from "./photos";
+import SaveIcon from "@material-ui/icons/Save";
+
 import TakePicture from "./components/Take_Picture";
 import { Button, Grid, GridList, GridListTile, GridListTileBar, IconButton } from "@material-ui/core";
 import getLocation from './getGeo'
@@ -12,8 +13,44 @@ class App extends Component {
     
     
     let photos = [...this.state.photos];
-    photos.push(val);
+    photos.push({imageData : val,selected : false});
     this.setState({photos})
+}
+saveImage = () =>{
+  this.state.photos.forEach(element => {
+    if(element.selected){
+      const xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          console.log("saved");
+        }
+      };
+      xhttp.open("POST", "http://localhost/getImage/index.php", false);
+      xhttp.setRequestHeader(
+        "Content-type",
+        "application/x-www-form-urlencoded"
+      );
+      xhttp.send(`imgData=${element.imageData}`);
+    }
+   
+  
+  });
+ 
+}
+selectImage = (imageContainer,index) => {
+  console.log()
+  if(!this.state.photos[index].selected){
+    this.refs[imageContainer].style.backgroundColor= 'red';
+    const photos = [...this.state.photos];
+    photos[index].selected = true;
+    this.setState({photos});
+  }else{
+    this.refs[imageContainer].style.backgroundColor= 'white';
+    const photos = [...this.state.photos];
+    photos[index].selected = false;
+    this.setState({photos});
+  }
+ 
 }
   render() {
     return (
@@ -24,16 +61,29 @@ class App extends Component {
         </Button>
 
         <p id="demo" />
-
+<p>{this.state.photos.length}</p>
+{this.state.photos.length > 0 ? (<IconButton
+              onClick={this.saveImage}
+              
+              aria-label="Delete"
+            >
+              <SaveIcon />
+            </IconButton>) : null}
         <GridList cellHeight={200}  cols={2}>
         {this.state.photos.map((tile,index) => (
           <GridListTile  key={index} cols={1}>
-            <img src={tile}  />
+          <div className="imageContainer" ref={`imageContainer${index}`} 
+          onClick={()=>this.selectImage(`imageContainer${index}`,index)}>
+          <img height="200" style={{maxWidth : '100%',maxHeight : '100%'}} src={tile.imageData}  />
+          </div>
+            
             
           </GridListTile>
         ))}
          
         </GridList>
+
+        
       </div>
     );
   }
--- src/components/Take_Picture.js
+++ src/components/Take_Picture.js
@@ -3,160 +3,144 @@ import IconButton from "@material-ui/core/IconButton";
 import { Dialog } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 import SaveIcon from "@material-ui/icons/Save";
-import {PhotoCamera, Close} from "@material-ui/icons/";
-
-
+import { PhotoCamera, Close } from "@material-ui/icons/";
 
 const styles = {
   container: {
     position: "relative",
     display: "block",
     height: "100%",
-    background: "#ddd",
+    background: "#ddd"
   },
   video: {
     position: "fixed",
     background: "#555",
-    top: '25%',
-    maxWidth: "100%",
-    
-   
+    top: "25%",
+    maxWidth: "100%"
   },
   canvas: {
     position: "absolute",
     opacity: 0
   },
   img: {
-    position: "absolute",
-    top: '25%',
-    opacity: 0
+    position: "fixed",
+    top: "25%",
+   
   }
 };
 class TakePicture extends Component {
   state = {
     open: false,
-    photos : this.props.photos
+    photos: this.props.photos
   };
 
   handleClickOpen = () => {
     this.setState({ open: true });
-  setTimeout(function () {
-    const video = document.getElementById("video"),
-    canvas = document.getElementById("canvas"),
-    capture  = document.getElementById("capture");
-  let width = 0;
-  let height = 0;
-  capture.style.opacity = 0 ;
- 
+    setTimeout(function() {
+      const video = document.getElementById("video"),
+        canvas = document.getElementById("canvas"),
+        capture = document.getElementById("capture");
+      let width = 0;
+      let height = 0;
+      capture.style.opacity = 0;
 
-  video.addEventListener("loadedmetadata", function() {
-    capture.style.opacity = 1 ;
-    width = this.videoWidth;
-    height = this.videoHeight;
-    canvas.setAttribute("width", width);
-    canvas.setAttribute("height", height);
-   // 
-    // ...
-    
-  });
-  navigator.getMedia =
-    navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia;
+      video.addEventListener("loadedmetadata", function() {
+        capture.style.opacity = 1;
+        width = this.videoWidth;
+        height = this.videoHeight;
+        canvas.setAttribute("width", width);
+        canvas.setAttribute("height", height);
+        //
+        // ...
+      });
+      navigator.getMedia =
+        navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia;
 
-  navigator.getMedia(
-    {
-      video: {width: {min: window.innerWidth}, height: {min: window.innerHeight}},
-      audio: false
-    },
-    function(stream) {
-      //
-      video.srcObject = stream;
-      video.play();
-    },
-    function(error) {
-      //error
-    }
-  );
-  },500);
-   
-    
+      navigator.getMedia(
+        {
+          video: {
+            width: { min: window.innerWidth },
+            height: { min: window.innerHeight }
+          },
+          audio: false
+        },
+        function(stream) {
+          //
+          video.srcObject = stream;
+          video.play();
+        },
+        function(error) {
+          //error
+        }
+      );
+    }, 500);
   };
 
   handleClose = () => {
     this.setState({ open: false });
   };
 
-
   handelCapture = () => {
     const video = document.getElementById("video"),
       canvas = document.getElementById("canvas"),
-      photo = document.getElementById("photo"),
-      context = canvas.getContext("2d"),
-      capture  = document.getElementById("capture");
-    
-      
+      span_img = document.getElementById("span_img"),
+      context = canvas.getContext("2d");
+      span_img.innerHTML ='';
+
+      var img = document.createElement("img");
+      span_img.appendChild(img);
+    context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+    this.props.sendData(canvas.toDataURL("image/png"));
+
+    console.log("capture");
+    img.setAttribute("src", canvas.toDataURL("image/png"));
+    img.setAttribute("width", window.innerWidth);
+
+    img.style.opacity = 1;
+    img.style.transition = "transform 1s";
+    img.style.transform = "scale(0,0)";
    
-      context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-
-      this.props.sendData(canvas.toDataURL("image/png"));
-
-      console.log("capture");
-       photo.setAttribute("src", canvas.toDataURL("image/png"));
-      photo.setAttribute("width", window.innerWidth);
-      
-      photo.style.opacity = 1; 
-/*-ms-transform: scale(0.5, 0.5); 
+    /*-ms-transform: scale(0.5, 0.5); 
 -webkit-transform: scale(0.5, 0.5);
 transform: scale(0.5, 0.5);*/
-  
-      let imgData = canvas.toDataURL("image/png");
-      var xhttp = new XMLHttpRequest();
-      var save_btn  = document.getElementById('save_btn');
-      save_btn.style.opacity = 1;
-     
-      save_btn.addEventListener('click',function(){
-        
-  
-        //save image
-        xhttp.onreadystatechange = function() {
-          if (this.readyState === 4 && this.status === 200) {
-            console.log("saved");
-          }
-        };
-        xhttp.open("POST", "http://localhost/getImage/index.php", false);
-        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhttp.send(`imgData=${imgData}`);
-  
-      }); 
-     
- 
+/* 
+    let imgData = canvas.toDataURL("image/png");
    
-  }
+    var save_btn = document.getElementById("save_btn");
+    save_btn.style.opacity = 1;
+ var xhttp = new XMLHttpRequest();
+    save_btn.addEventListener("click", function() {
+      //save image
+      xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          console.log("saved");
+        }
+      };
+      xhttp.open("POST", "http://localhost/getImage/index.php", false);
+      xhttp.setRequestHeader(
+        "Content-type",
+        "application/x-www-form-urlencoded"
+      );
+      xhttp.send(`imgData=${imgData}`);
+    }); */
+  };
   render() {
     const { classes } = this.props;
     return (
       <div>
-        <Dialog
-         
-          fullScreen
-          open={this.state.open}
-          onClose={this.handleClose}
-        >
+        <Dialog fullScreen open={this.state.open} onClose={this.handleClose}>
           <div className={classes.container}>
             <video className={classes.video} id="video" autoPlay />
 
             <canvas className={classes.canvas} id="canvas" />
-            <img
-              src="https://lh3.googleusercontent.com/a-/AN66SAzZVnfOaFEnqIWPvFKpO4JFUQB9t5qeXIBKqD70=s32-cc"
-              className={classes.img}
-              id="photo"
-            />
+            <span id="span_img" className={classes.img} />
+
             <IconButton
               onClick={this.handleClose}
               style={{ position: "absolute" }}
-              
               aria-label="Close"
             >
               <Close />
@@ -166,7 +150,7 @@ transform: scale(0.5, 0.5);*/
               onClick={this.handelCapture}
               id="capture"
             >
-             <PhotoCamera />
+              <PhotoCamera />
             </IconButton>
             <IconButton
               id="save_btn"
